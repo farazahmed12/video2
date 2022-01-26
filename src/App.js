07c3f87afaa178182f4cec4649733624a4d8d4a4
farazahmed12 <masterdot707@gmail.com>
2022-01-26 19:38:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,55 @@
-import React, { useState } from 'react'
-import Accordion from './components/accordion'
-import Search from './components/search'
-import Dropdown from './components/dropdown'
-import Translate from './components/translate'
+import React, { useState } from "react";
+import Accordion from "./components/accordion";
+import Search from "./components/search";
+import Dropdown from "./components/dropdown";
+import Translate from "./components/translate";
 
 const items = [
   {
-    title: 'What is React',
-    content: 'React is frontend javascript framework'
+    title: "What is React",
+    content: "React is frontend javascript framework",
   },
   {
-    title: 'Why use React ?',
-    content: 'React is favourate'
+    title: "Why use React ?",
+    content: "React is favourate",
   },
   {
-    title: 'How do you use React',
-    content: 'React is used by creating components, Hooks and Redux'
-  }
-]
+    title: "How do you use React",
+    content: "React is used by creating components, Hooks and Redux",
+  },
+];
 
 const options = [
   {
-    label: 'The Color Red',
-    value: 'red'
+    label: "The Color Red",
+    value: "red",
   },
   {
-    label: 'The Color Green',
-    value: 'green'
+    label: "The Color Green",
+    value: "green",
   },
   {
-    label: 'The Shade of Blue',
-    value: 'blue'
-  }
-]
-
+    label: "The Shade of Blue",
+    value: "blue",
+  },
+];
 
 function App() {
-  const [selected, setSelected] = useState(options[0])
-  
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div className="App">
+      <h1>Widget App</h1>
       <br />
-      
-      <Dropdown 
-      label={'Select a Language'}
-      selected={selected}
-      onSelectedChange={setSelected}
-      options={options} 
-      />  
+
+      <Dropdown
+        label={"Select a Language"}
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
 
       <Translate />
-      
     </div>
   );
 }
